8c1ffbda7d656b71cb979ba3e7f1afb52b21046c
Sohan Puthran <puthran.sohan@gmail.com>
2025-05-16 20:19:11 -0500

    Bollinger Bands implementation

--- technical_indicators/ATR.py
+++ technical_indicators/ATR.py
@@ -6,6 +6,12 @@ import matplotlib.pyplot as plt
 def implement_ATR(df, period=14):
     """
     Calculate Average True Range (ATR)
+    Parameters:
+    - df (DataFrame) : DataFrame containing 'High', 'Low', and 'Close' prices
+    - period (int) : The number of periods to calculate the ATR
+
+    Returns:
+    - atr (Series) : Series containing the ATR values
     """
     df['High-Low'] = df['High'] - df['Low']
     df['High-Prev Close'] = abs(df['High'] - df['Close'].shift(1))
@@ -14,9 +20,15 @@ def implement_ATR(df, period=14):
     atr = df['True Range'].rolling(window=period).mean()
     return atr
 
-def ATR_plot(df, ticker, period=14):
+def ATR_plot(df, ticker):
     """
     Plot ATR
+    Parameters:
+    - df (DataFrame) : DataFrame containing 'High', 'Low', and 'Close' prices
+    - ticker (str) : Ticker symbol for the stock
+
+    Returns:
+    - None : Displays the ATR plot
     """
     plt.figure(figsize=(12, 6))
     plt.plot(df.index, df['ATR'], label='ATR', color='blue')
--- technical_indicators/MACD.py
+++ technical_indicators/MACD.py
@@ -1,8 +1,20 @@
+# Implementation of Moving Average Convergence Divergence (MACD) indicator
+# MACD is a trend-following momentum indicator that shows the relationship between two moving averages of a security’s price.
+
 import matplotlib.pyplot as plt
 
 def implement_MACD(data, short_window=12, long_window=26, signal_window=9):
     """
     Calculate MACD and Signal Line
+    Parameters:
+    - data (DataFrame) : DataFrame containing 'Close' prices
+    - short_window (int) : The short-term EMA period
+    - long_window (int) : The long-term EMA period
+    - signal_window (int) : The signal line EMA period
+
+    Returns:
+    - macd (Series) : Series containing the MACD values
+    - signal_line (Series) : Series containing the Signal Line values
     """
     # Calculate the short-term and long-term EMAs
     short_ema = data['Close'].ewm(span=short_window, adjust=False).mean()
@@ -20,6 +32,12 @@ def implement_MACD(data, short_window=12, long_window=26, signal_window=9):
 def plot_macd(data, ticker):
     """
     Plot MACD and Signal Line
+    Parameters:
+    - data (DataFrame) : DataFrame containing 'Close' prices and MACD values
+    - ticker (str) : Ticker symbol for the stock
+
+    Returns:
+    - None : Displays the MACD plot
     """
     fig, ax = plt.subplots(figsize=(15, 8))
     ax.plot(data['MACD'], label='MACD', color='blue')
