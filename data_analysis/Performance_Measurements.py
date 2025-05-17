a894a4d7312128d19567a9d2c75fa3577a30c987
Sohan Puthran <puthran.sohan@gmail.com>
2025-05-17 17:35:57 -0500

    Implementation of CAGR

---
+++ data_analysis/Performance_Measurements.py
@@ -0,0 +1,24 @@
+# Implementing the Performance Measurements
+# This module contains functions to calculate various performance metrics for financial data.
+
+# Implementation of Cumulative Annual Growth Rate (CAGR)
+def CAGR(df, period=1):
+    """
+    Calculate the Cummulative Annual Growth Rate (CAGR) of a given DataFrame.
+
+    Parameters:
+    df (pd.DataFrame): DataFrame containing the data.
+    period (int): The number of periods to calculate CAGR for.
+
+    Returns:
+    float: The CAGR value.
+    """
+    # Calculate the CAGR
+    df['return'] = df['Close'].pct_change()
+    df['cumulative_return'] = (1 + df['return']).cumprod()
+    cagr = (df['cumulative_return'].iloc[-1] ** (1 / period)) - 1
+    # Drop the 'return' and 'cumulative_return' columns
+    df.drop(columns=['return', 'cumulative_return'], inplace=True)
+    # Return the CAGR value
+    return cagr
+
