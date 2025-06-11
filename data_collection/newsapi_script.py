237707868c4506ce6a3f3e84e6739d10ea753821
Sohan Puthran <puthran.sohan@gmail.com>
2025-06-11 14:09:31 -0500

    Bug Fix: Removed the "time from" parameter for the NEWSAPI query.

--- data_collection/newsapi_script.py
+++ data_collection/newsapi_script.py
@@ -1,4 +1,4 @@
-import requests, pandas as pd, re
+import requests, pandas as pd, re, pytz
 from datetime import datetime, timedelta
 
 import sys
@@ -12,13 +12,21 @@ from utils.config import NEWS_API_KEY
 
 API_KEY = NEWS_API_KEY
 QUERY = "FDA OR earnings OR acquisition OR upgrade OR contract"
-LIMIT = 50
-date_str = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+# 100 is the maximum page size for NewsAPI
+LIMIT = 100
+# date_str = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+
+# Function to get the current time in Zulu (UTC) ISO 8601 format for the newsapi query
+# def get_current_zulu_time():
+#     """Returns the current time in Zulu (UTC) ISO 8601 format."""
+#     utc_now = datetime.now(pytz.utc)
+#     return utc_now.strftime('%Y-%m-%dT%H:%M:%SZ')
+
 def get_general_news():
     url = "https://newsapi.org/v2/everything"
     params = {
         "q": QUERY,
-        "from": date_str, # datetime.now().strftime('%Y-%m-%d'),
+        # "from": get_current_zulu_time(),
         "language": "en",
         "sortBy": "publishedAt",
         "pageSize": LIMIT,
@@ -33,24 +41,20 @@ def get_general_news():
     else:
         return response.json().get("articles", [])
 
-def extract_tickers(text):
-    return re.findall(r'\b[A-Z]{2,5}\b', text)
-
 def build_catalyst_news_list():
     articles = get_general_news()
     rows = []
     for a in articles:
         title, desc = a.get("title",""), a.get("description","")
-        tickers = set(extract_tickers(title + " " + desc))
+
         rows.append({
             "publishedAt": a.get("publishedAt"),
             "source": a.get("source",{}).get("name"),
             "title": title,
             "description": desc,
-            "url": a.get("url"),
-            "tickers": ", ".join(tickers)
+            "url": a.get("url")
         })
     df = pd.DataFrame(rows)
-    out = f"catalyst_news_{datetime.now().strftime('%Y-%m-%d')}.csv"
+    out = f"../data/catalyst_news_{datetime.now().strftime('%Y-%m-%d')}.csv"
     df.to_csv(out, index=False)
     print("Saved:", out)
